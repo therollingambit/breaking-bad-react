071a36b0c2f581eb3896510e4d4d9500a5359a67
therollingambit <therollingambit@gmail.com>
2021-04-28 01:13:42 +0800

    added search input

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Header from './components/ui/Header';
-import CharacterGrid from './components/characters/CharacterGrid';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from "./components/ui/Search";
+import "./App.css";
 
 const App = () => {
   const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true); //true by default
+  const [query, setQuery] = useState("");
 
   //make a request
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      );
 
       setItems(result.data);
       setIsLoading(false);
-    }
+    };
 
-    fetchItems();
-  }, [])
+    fetchItems(); //call the function to fetch
+  }, [query]); //whenever query change, useEffect will fireoff again
 
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setQuery(q)} /> {/* setQuery is part of state */}
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import CharacterItem from './CharacterItem'
+import React from 'react';
+import CharacterItem from './CharacterItem';
+import Spinner from '../ui/Spinner';
 
 const CharacterGrid = ({ items, isLoading }) => { //destructure props.items and props.isLoading
-    return isLoading ? (<h1>Loading...</h1>) : (<section className="cards">
+    return isLoading ? (<Spinner />) : (<section className="cards">
         {items.map(item => (
             <CharacterItem key={item.char_id} item={item} />
         ))}
     </section>)
 }
 
-export default CharacterGrid
+export default CharacterGrid;
